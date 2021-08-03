9445b6a3fe847235a12def8494d735c3d5886b76
rm3na <84992903+rm3na@users.noreply.github.com>
2021-08-02 18:45:21 -0600

    Update main.tf

--- virtual-machines/main.tf
+++ virtual-machines/main.tf
@@ -1,5 +1,15 @@
+  
+provider "azurerm" {
+  version = "<=1.34.0"
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
+
+
 data "terraform_remote_state" "vnet" {
-  backend = "local"
+  backend = "remote"
 
   config = {
     organization = "Nterone"
